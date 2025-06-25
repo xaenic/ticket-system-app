d35681ffcb20115990e1e5e465f4d913c5ba1cec
xaenic <allanvillegas35@gmail.com>
2025-06-25 15:01:29 +0800

    feat: Add role-based access control with RoleMiddleware and update user route

---
+++ src/backend/database/migrations/2025_06_25_064733_add_tickets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+         Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('status');
+            $table->string('priority');
+
+            $table->foreignId('client_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('department_id')->constrained('departments')->onDelete('cascade');
+            $table->foreignId('assigned_user_id')->nullable()->constrained('users')->onDelete('set null');
+           
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
--- src/backend/routes/api.php
+++ src/backend/routes/api.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\Auth\AuthController;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware(['auth:api', 'role:admin'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
