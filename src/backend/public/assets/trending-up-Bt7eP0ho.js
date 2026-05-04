b641a4b1d4d4101de2f1c26a6477f2e7eabc7bbd
xaenic <allanvillegas35@gmail.com>
2026-05-05 01:27:00 +0800

    fix:deprecate

---
+++ src/backend/public/assets/trending-up-Bt7eP0ho.js
@@ -0,0 +1,11 @@
+import{c}from"./index-Bb_ZrYSj.js";/**
+ * @license lucide-react v0.523.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M21.801 10A10 10 0 1 1 17 3.335",key:"yps3ct"}],["path",{d:"m9 11 3 3L22 4",key:"1pflzl"}]],n=c("circle-check-big",e);/**
+ * @license lucide-react v0.523.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"M16 7h6v6",key:"box55l"}],["path",{d:"m22 7-8.5 8.5-5-5L2 17",key:"1t1m79"}]],i=c("trending-up",t);export{n as C,i as T};
